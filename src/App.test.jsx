b7f9bf99ecb565c081882d6eb3e8a79c3b7996ba
Alex Wilkes <awilkes2016@gmail.com>
2024-03-19 23:22:12 -0400

    Added configuration to run tests during ci

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import {describe, it, expect} from 'vitest'
+import {render} from "@testing-library/react";
+import App from "./App.jsx";
+
+describe("App", () => {
+    it('should render logos', () => {
+        const {getByTestId} = render(<App />)
+        expect(getByTestId("logos")).toBeVisible()
+    })
+})
